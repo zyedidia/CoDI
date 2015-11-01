33122cb08fefdbe0e1f78d2e93ca9c5146f84bd2
Liam Yanulis <liam.yanulis@concordacademy.org>
2015-11-01 11:50:24 -0500

    Remove comment

--- src/image_proc/app.d
+++ src/image_proc/app.d
@@ -1,7 +1,6 @@
 import std.stdio;
 import imageformats;
 
-// finds the dominant color in an input picture
 void main(in string[] args) {
     if (args.length < 2) {
         writeln("No input file");
