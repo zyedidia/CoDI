6e9b6a255466ca87deeb19773651bc16539dfee4
Zachary Yedidia <zyedidia@gmail.com>
2015-11-08 11:21:09 -0500

    Calculate avg max min and stddev

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ mkdir CoDI.app/Contents/Resources
 cp .codi-app.sh CoDI.app/Contents/MacOS/codi
 chmod +x CoDI.app/Contents/MacOS/codi
 cp -r src CoDI.app/Contents/Resources/
-
-mv CoDI.app/Contents/Resources/src/.release.json CoDI.app/Contents/Resources/src/package.json
-rm CoDI.app/Contents/Resources/src/.debug.json
--- src/image_proc/app.d
+++ src/image_proc/app.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.math;
 import imageformats;
 
 void main(in string[] args) {
@@ -16,6 +17,11 @@ void main(in string[] args) {
     auto greenPixels = 0;
     auto bluePixels = 0;
 
+    double avgR = 0; double avgG = 0; double avgB = 0;
+    int minR = 255; int minG = 255; int minB = 255;
+    int maxR = 0; int maxG = 0; int maxB = 0;
+    double stddevR = 0; double stddevG = 0; double stddevB = 0;
+
     foreach (i; 0 .. im.pixels.length) {
         auto rgb = i % 3;
         if (rgb == 0) {
@@ -28,16 +34,72 @@ void main(in string[] args) {
 
         if (r > 25) {
             redPixels++;
+            avgR += r;
+
+            if (r < minR) {
+                minR = r;
+            } else if (r > maxR) {
+                maxR = r;
+            }
         }
 
         if (g > 25) {
             greenPixels++;
+            avgG += g;
+            
+            if (g < minG) {
+                minG = g;
+            } else if (g > maxG) {
+                maxG = g;
+            }
         }
 
         if (b > 25) {
             bluePixels++;
+            avgB += b;
+
+            if (b < minB) {
+                minB = b;
+            } else if (b > maxB) {
+                maxB = b;
+            }
         }
     }
+
+    avgR /= redPixels;
+    avgG /= greenPixels;
+    avgB /= bluePixels;
+
+    foreach (i; 0 .. im.pixels.length) {
+        auto rgb = i % 3;
+
+        if (rgb == 0) {
+            r = im.pixels[i];
+        } else if (rgb == 1) {
+            g = im.pixels[i];
+        } else if (rgb == 2) {
+            b = im.pixels[i];
+        }
+
+        if (r > 25) {
+            stddevR += (r - avgR) * (r - avgR);
+        } 
+        if (g > 25) {
+            stddevG += (g - avgG) * (g - avgG);
+        } 
+        if (b > 25) {
+            stddevB += (b - avgB) * (b - avgB);
+        }
+    }
+
+    stddevR /= redPixels;
+    stddevR = sqrt(stddevR);
+
+    stddevG /= greenPixels;
+    stddevG = sqrt(stddevG);
+
+    stddevB /= bluePixels;
+    stddevB = sqrt(stddevB);
 
     auto dominantColor = 0;
     if (redPixels > greenPixels) {
@@ -52,8 +114,23 @@ void main(in string[] args) {
     }
 
     writeln("Dominant color is ", dominantColor);
-
-    writeln("Size is ", dominantColor == 0 ? redPixels : dominantColor == 1 ? greenPixels
-        : bluePixels);
+    writeln();
+    writeln("Size is ", dominantColor == 0 ? redPixels : dominantColor == 1 ? greenPixels : bluePixels);
+    writeln();
+    writeln("Average R: ", avgR);
+    writeln("Average G: ", avgG);
+    writeln("Average B: ", avgB);
+    writeln();
+    writeln("Min R: ", minR);
+    writeln("Min G: ", minG);
+    writeln("Min B: ", minB);
+    writeln();
+    writeln("Max R: ", maxR);
+    writeln("Max G: ", maxG);
+    writeln("Max B: ", maxB);
+    writeln();
+    writeln("StdDev R: ", stddevR);
+    writeln("StdDev G: ", stddevG);
+    writeln("StdDev B: ", stddevB);
     /* write_image("out.png", im.w, im.h, im.pixels); */
 }
